59ba59e7b9bac41b7a497440f4af310489a5b336
Kevin Mathias <komathias1@gmail.com>
2020-03-29 20:15:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,10 @@
 # Add  code here!
 
 def prime?(num)
-  
   n = 2
   while n < num
-  return false if num % n ==0
-    n = n + 1
-  end
-  true
+  return false if num % n == 0
+  n += 1
+end
+true
 end
